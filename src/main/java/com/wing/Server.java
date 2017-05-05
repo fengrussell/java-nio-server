ad4b2b6ad8651eda24cfbf143e26b4268dc7fe66
russell <russell_feng@163.com>
2017-05-05 23:10:35 +0800

    add server.java

---
+++ src/main/java/com/wing/Server.java
@@ -0,0 +1,10 @@
+package com.wing;
+
+/**
+ * Created by russell on 2017/5/5.
+ */
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
